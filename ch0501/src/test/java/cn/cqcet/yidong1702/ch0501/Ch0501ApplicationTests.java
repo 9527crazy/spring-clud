27cda86dc46a72fea2df579d53ec618ab00d8c91
9527crazy <2840161508@qq.com>
2019-09-27 13:15:12 +0800

    ribbon服务间的均衡负载

---
+++ ch0501/src/test/java/cn/cqcet/yidong1702/ch0501/Ch0501ApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.cqcet.yidong1702.ch0501;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ch0501ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
