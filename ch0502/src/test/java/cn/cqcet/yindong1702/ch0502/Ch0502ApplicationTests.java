688ad611d713f3a1a4129865f010442df94903b4
9527crazy <2840161508@qq.com>
2019-09-29 11:38:34 +0800

    Gatway ApiZuul

---
+++ ch0502/src/test/java/cn/cqcet/yindong1702/ch0502/Ch0502ApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.cqcet.yindong1702.ch0502;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ch0502ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
